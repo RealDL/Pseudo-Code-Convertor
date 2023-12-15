fc4be4c46395160e34326c4e269a30bafaad7043
DL <91336924+RealDL@users.noreply.github.com>
2023-12-15 10:24:45 +0000

    Update version.py

--- version.py
+++ version.py
@@ -1,24 +1,29 @@
-import re
+import string
 
 class PythonToPseudocode:
     def __init__(self):
         self.python_processing = self.get_file()
         self.by_hand = []
+        self.variables = []
+        self.remove_spaces()
         self.comments()
         self.iteration()
         self.selection()
         self.files()
+        self.self_variables()
+        self.functions_and_procedures()
         self.classes()
         self.try_except()
+        
         for line in self.python_processing:
             print(line)
 
     def get_file(self):
-        
+       
         with open("MyPythonFile.txt","r") as my_file:
             processing = [line.strip('\n') for line in my_file]
         return processing
-    
+   
     def code_indent(self,linestart):
         spaces = 0
         linecheck = self.python_processing[linestart]
@@ -43,12 +48,26 @@ class PythonToPseudocode:
             self.by_hand.append(byhandadd)
         z=(" "*start_spaces)+endstatement
         self.python_processing.insert(current_line,z)
-    
+   
+    def remove_spaces(self):
+        print(self.python_processing)
+        string_identities = string.ascii_letters + string.digits + string.punctuation
+        for index, line in enumerate(self.python_processing):
+            to_delete = False
+            if line == "":
+                to_delete = True
+                
+            if line.isspace():
+                to_delete = True
+
+            if to_delete == True:
+                del self.python_processing[index]    
+
     def comments(self):
         for index, line in enumerate(self.python_processing):
             if "#" in line:
                 self.python_processing[index] = line.replace("#","//")
-    
+   
     def iteration(self):
         for index, line in enumerate(self.python_processing):
             if "else:" in line:
@@ -73,7 +92,7 @@ class PythonToPseudocode:
                     line = line.replace(":","")
                     self.ending_insert(index,"endwhile")
             self.python_processing[index] = line
-    
+   
     def selection(self):
         for index, line in enumerate(self.python_processing):
             if " and " in line:
@@ -108,7 +127,18 @@ class PythonToPseudocode:
             if "write" in line:
                 line = line.replace("write","writeLine")
             self.python_processing[index] = line
-    
+   
+    def self_variables(self):
+        for index, line in enumerate(self.python_processing):
+            if "self." in line and "=" in line:
+                if line.find("=") > line.find("self."):
+                    words = line.split()  # Split the line into words
+                    words = words[0]
+                    if not "[" in words and not "]" in words:
+                        words = words.replace("self.","")
+                        if words not in self.variables:
+                            self.variables.append(words)
+
     def classes(self):
         for index, line in enumerate(self.python_processing):
             if "class" in line:
@@ -122,18 +152,27 @@ class PythonToPseudocode:
                     if "()" in line:
                         line = line.replace("()","")
                     line = line.replace(":","")
+                for index_range, variable in enumerate(self.variables):
+                    number_of_spaces = self.code_indent(index+1)
+                    spaces = " "*number_of_spaces
+                    space = ""
+                    if index_range == 0:
+                        space = "\n"
+                    self.python_processing.insert(index+1, f"{spaces}private {variable}{space}")
+
             if "=" in line and "(" in line and ")" in line:
                 if "= " in line:
                     line = line.replace("=","= new")
                 else:
                     line = line.replace("=","= new ")
-            
+           
             if "def __init__(" in line:
                 if "self" in line:
                     line = line.replace("self","")
                 if ":" in line:
                     line = line.replace(":","")
                 line = line.replace("def __init__(","public procedure new(")
+                self.ending_insert(index,"endprocedure")
 
             if "self" in line:
                 if "self," in line:
@@ -145,7 +184,7 @@ class PythonToPseudocode:
                     line = line.replace("self.","")
                 if "self" in line:
                     line = line.replace("self","")
-                
+               
             self.python_processing[index] = line
 
     def try_except(self):
@@ -156,17 +195,61 @@ class PythonToPseudocode:
                 if "except" in line:
                     self.ending_insert(index,"endtry")
             self.python_processing[index] = line
-                
-    
+               
+   
     def functions_and_procedures(self):
         for index, line in enumerate(self.python_processing):
+            type_of_procedure = ""
             if "def" in line:
-                pass
+                if "self" in line:
+                    type_of_procedure = "class"
+                else:
+                    type_of_procedure = "normal"
+                if "def __init__(" not in line:
+                    lines = 1
+                    type_of_def = ""
+                    found = False
+                    while found != True:
+                        try:
+                            if "def" in self.python_processing[index+lines]: 
+                                if "//" in line:
+                                    if line.find("//") > line.find("def"):
+                                        found = True
+                                        type_of_def = "procedure"
+
+                                else:
+                                    found = True
+                                    type_of_def = "procedure"
+                            elif "return" in self.python_processing[index+lines]:
+                                if "//" in line:
+                                    if line.find("//") > line.find("return"):
+                                        found = True
+                                        type_of_def = "function"
+
+                                else:
+                                    found = True
+                                    type_of_def = "function"
+                        except:
+                            # List out of range
+                            found = True
+                            type_of_def = "procedure"
+                        lines += 1
+
+                    if type_of_def == "procedure":
+                        if type_of_procedure == "class":
+                            line = line.replace("def","public procedure")
+                        elif type_of_procedure == "normal":
+                            line = line.replace("def","procedure")
+                        self.ending_insert(index,"endprocedure")
+                    elif type_of_def == "function":
+                        line = line.replace("def","function")
+                        self.ending_insert(index,"endfunction")
+                    if ":" in line:
+                        line = line.replace(":","")
+                    self.python_processing[index] = line
 
             # found out whether function or procedure.
             # if found return, then its a function, if found
             # no return its a procedure.
-
-            
 
 converter = PythonToPseudocode()
